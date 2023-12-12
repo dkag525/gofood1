27d1356cd3deec1593922255dafdf15eadb62a26
dkag525 <dkag525@gmail.com>
2023-12-12 23:39:45 +0530

    added

--- Backend/Routes/CreateUser.js
+++ Backend/Routes/CreateUser.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/User/User");
+const { query, validationResult } = require("express-validator");
 
-router.post("/CreateUser", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-      location: req.body.location,
-    });
-    res.json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false });
+router.post(
+  "/CreateUser",
+  [
+    body("email").isEmail(),
+    body("name").isEmail(),
+    body("password", "Incorrect Password").isLength({ min: 5 }),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
   }
-});
+);
 
 module.exports = router;
